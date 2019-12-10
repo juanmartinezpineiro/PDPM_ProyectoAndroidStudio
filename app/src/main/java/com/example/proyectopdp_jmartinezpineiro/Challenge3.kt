a69d1ddbaf84d8a1798d0e05de4c88512c98383c
Juan Martínez <jmartinezpineiro@danielcastelao.org>
2019-12-10 21:35:47 +0100

    Inicio del desarrollo

--- app/src/main/java/com/example/proyectopdp_jmartinezpineiro/Challenge3.kt
+++ app/src/main/java/com/example/proyectopdp_jmartinezpineiro/Challenge3.kt
@@ -3,13 +3,10 @@ package com.example.proyectopdp_jmartinezpineiro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Challenge3 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        
-
+        setContentView(R.layout.activity_challenge3)
     }
 }
