0c2093b649ecac3e03289fa7f8da0640e6da1d90
Juan Martínez <jmartinezpineiro@danielcastelao.org>
2019-12-10 21:03:30 +0100

    Primera toma de contacto!

---
+++ app/src/main/java/com/example/proyectopdp_jmartinezpineiro/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.proyectopdp_jmartinezpineiro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
